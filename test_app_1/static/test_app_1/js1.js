81eabe97c301575a9488e683af9bc9e854007cad
bldev2473 <bldev2473@gmail.com>
2021-01-04 16:38:02 +0900

    Setup project

---
+++ test_app_1/static/test_app_1/js1.js
@@ -0,0 +1,6 @@
+var g_var = 'js1 variable'
+console.log(g_var)
+
+function printVar() {
+    console.log(g_var)
+}
---
+++ test_app_1/static/test_app_1/js2.js
@@ -0,0 +1,6 @@
+var g_var = 'js2 variable'
+console.log(g_var)
+
+function printVar() {
+    console.log(g_var)
+}
